a813ec6ccc9fb4aca4ec0a6aea6c6f9f356080fc
Res-Nah <1154912914@qq.com>
2024-08-12 22:17:12 +0800

    2024.08.12

---
+++ 142.环形链表-ii.java
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=java
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) {
+                ListNode index1 = fast;
+                ListNode index2 = head;
+                while(index1 != index2) {
+                    index1 = index1.next;
+                    index2 = index2.next;
+                }
+                return index1;
+            }
+        }
+        return null;
+    }
+}
+// @lc code=end
+
---
+++ 24.两两交换链表中的节点.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=java
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode pre = dummy;
+        while(pre.next != null && pre.next.next != null) {
+            ListNode temp1 = pre.next;
+            ListNode temp2 = pre.next.next;
+            pre.next = temp2;
+            temp1.next = temp2.next;
+            temp2.next = temp1;
+            pre = pre.next.next;
+        }
+        return dummy.next;
+    }
+}
+// @lc code=end
+
