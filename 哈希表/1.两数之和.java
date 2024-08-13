ca210e7a3ce61ca069a3571811b24347d3cf277c
Res-Nah <1154912914@qq.com>
2024-08-13 23:38:04 +0800

    2024.08.13

---
+++ 哈希表/1.两数之和.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        if(nums == null || nums.length == 0) {
+            return res;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if(map.containsKey(temp)) {
+                res[0] = i;
+                res[1] = map.get(temp);
+                break;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
+    }
+}
+// @lc code=end
+
---
+++ 哈希表/349.两个数组的交集.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=349 lang=java
+ *
+ * [349] 两个数组的交集
+ */
+
+// @lc code=start
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if(nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return new int[0];
+        }
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> result = new HashSet<>();
+        for(int i : nums1) {
+            set1.add(i);
+        } 
+        for(int i : nums2) {
+            if(set1.contains(i)) {
+                result.add(i);
+            }
+        }
+        int[] array = new int[result.size()];
+        int j = 0;
+        for(int i : result) {
+            array[j++] = i;
+        }
+        return array;
+    }
+}
+// @lc code=end
+
