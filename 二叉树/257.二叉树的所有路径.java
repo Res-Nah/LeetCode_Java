d659665d1735aa6fd818baf15324de9d915a5479
Res-Nah <1154912914@qq.com>
2024-09-04 22:21:54 +0800

    2024.09.04

---
+++ 二叉树/257.二叉树的所有路径.java
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=257 lang=java
+ *
+ * [257] 二叉树的所有路径
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        List<Integer> paths = new ArrayList<>();
+        traversal(root, paths, res);
+        return res;
+    }
+
+    private void traversal(TreeNode root, List<Integer> paths, List<String> res) {
+        paths.add(root.val);
+        if (root.left == null && root.right == null) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < paths.size() - 1; i++) {
+                sb.append(paths.get(i)).append("->");
+            }
+            sb.append(paths.get(paths.size() - 1));
+            res.add(sb.toString());
+            return;
+        }
+        if (root.left != null) { 
+            traversal(root.left, paths, res);
+            paths.remove(paths.size() - 1);
+        }
+        if (root.right != null) { 
+            traversal(root.right, paths, res);
+            paths.remove(paths.size() - 1);
+        }
+    }
+}
+// @lc code=end
+
